8e0997936281bc337ad9e11c63673e36ed0b02b5
Nick Volpe <navolpe@gmail.com>
2021-07-05 12:35:23 -0700

    test

--- App.tsx
+++ App.tsx
@@ -7,6 +7,8 @@ import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
+// test commit
+
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
